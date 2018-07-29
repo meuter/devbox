602402be8dd1262bb70488b014c074455e8b8b32
Cédric Meuter <Cdric Meuter>
2018-07-29 15:47:40 +0200

    debug install_i3

--- scripts/install_i3_gaps.sh
+++ scripts/install_i3_gaps.sh
@@ -53,7 +53,7 @@ git clone git://github.com/tobi-wan-kenobi/bumblebee-status ~/.config/i3/bumbleb
 if [ -f ~/.config/i3/config ]; then
   mv ~/.config/i3/config ~/.config/i3/config.bak
 else
-  mkdir -p ̣~/.config/i3/
+  mkdir -p ~/.config/i3
 fi
 ln -s ~/.dotfiles/i3/config ~/.config/i3/config
 
