c2c4215ce33855db56af109bbc637957037e3ade
Cédric Meuter <Cdric Meuter>
2018-07-29 12:31:40 +0200

    VBoxLinuxAdditions.run returns 2 as exit code??

--- scripts/install_guest_additions.sh
+++ scripts/install_guest_additions.sh
@@ -5,7 +5,7 @@ sudo apt install -y dkms build-essential linux-headers-generic \
 
 sudo mkdir -p /media/cdrom
 sudo mount -o loop /usr/share/virtualbox/VBoxGuestAdditions.iso /media/cdrom
-sudo /media/cdrom/VBoxLinuxAdditions.run
+sudo /media/cdrom/VBoxLinuxAdditions.run && true
 sudo umount /media/cdrom
 sudo rmdir /media/cdrom
 
