3bb6b9c4275e94b39e48df7dec4c3ca896d0f042
Cédric Meuter <Cdric Meuter>
2018-07-29 13:16:57 +0200

    install guest addition no interaction from oracle's ppa

--- scripts/install_guest_additions.sh
+++ scripts/install_guest_additions.sh
@@ -1,10 +1,13 @@
 #! /bin/bash -e
 
+wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
+sudo sh -c "echo 'deb https://download.virtualbox.org/virtualbox/debian/ bionic non-free contrib' > /etc/apt/sources.list.d/virtualbox.list"
+sudo apt-get update
 sudo apt install -y dkms build-essential linux-headers-generic linux-headers-$(uname -r) 
 
-sudo mkdir -p /media/cdrom
-sudo mount /dev/cdrom /media/cdrom
-sudo /media/cdrom/VBoxLinuxAdditions.run && true
-sudo umount /media/cdrom
-sudo rmdir /media/cdrom
+sudo mkdir -p /media/additions
+sudo mount -o loop /usr/share/virtualbox/VBoxGuestAdditions.iso /media/additions
+sudo /media/additions/VBoxLinuxAdditions.run && true
+sudo umount /media/additions
+sudo rmdir /media/additions
 
