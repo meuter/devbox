a809d019e5bf7e64c086d50f597f218ddd877cbb
Cédric Meuter <Cdric Meuter>
2018-07-29 14:28:11 +0200

    debug guest addition

--- scripts/install_guest_additions.sh
+++ scripts/install_guest_additions.sh
@@ -4,6 +4,7 @@ wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-
 sudo sh -c "echo 'deb https://download.virtualbox.org/virtualbox/debian/ bionic non-free contrib' > /etc/apt/sources.list.d/virtualbox.list"
 sudo apt update
 sudo apt install -y dkms build-essential linux-headers-generic linux-headers-$(uname -r) 
+sudo apt install -y virtualbox-5.2
 
 sudo mkdir -p /media/additions
 sudo mount -o loop /usr/share/virtualbox/VBoxGuestAdditions.iso /media/additions
