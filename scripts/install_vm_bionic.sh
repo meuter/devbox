e52fffa7b190584d6379c4f00b0ca9514a29d561
Cédric Meuter <Cdric Meuter>
2018-07-29 11:48:50 +0200

    guest addition first

--- scripts/install_vm_bionic.sh
+++ scripts/install_vm_bionic.sh
@@ -1,12 +1,13 @@
 #! /bin/bash
 
+./install_guest_additions.sh
+
 # replace default source.list which does not have universe and multiverse
 # by default on ubuntu server
 sudo cp /etc/apt/sources.list /etc/apt/sources.list.bak
 sudo cp ~/.dotfiles/ubuntu/bionic.list /etc/apt/sources.list
 sudo apt-get update
 
-./install_guest_additions.sh
 ./install_fonts.sh
 ./install_urxvt.sh
 ./install_i3_gaps.sh
