36a21c6712cf7e8d8d6b5d8399333a9d4d94a4fa
Cédric Meuter <Cdric Meuter>
2018-07-29 11:43:38 +0200

    zsh debug

--- scripts/install_zsh.sh
+++ scripts/install_zsh.sh
@@ -5,10 +5,10 @@ sudo apt-get install zsh wget
 sudo usermod -s $(which zsh) $(whoami)
 
 # install oh-my-zsh
-sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.config/oh-my-zsh/
 
 # install bullet train theme
-wget http://raw.github.com/caiogondim/bullet-train-oh-my-zsh-theme/master/bullet-train.zsh-theme -P ~/.oh-my-zsh/custom/themes/
+wget http://raw.github.com/caiogondim/bullet-train-oh-my-zsh-theme/master/bullet-train.zsh-theme -P ~/.config/oh-my-zsh/themes/
 
 # update zsh simlink
 if [ -f ~/.zshrc ]; then
