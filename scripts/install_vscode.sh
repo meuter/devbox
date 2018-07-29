3e4d942985bc234e49eaf273510f47d595e081cb
Cédric Meuter <Cdric Meuter>
2018-07-29 16:38:18 +0200

    cleanup i3 config + install code extension

--- scripts/install_vscode.sh
+++ scripts/install_vscode.sh
@@ -6,5 +6,18 @@ curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microso
 sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
 sudo apt update
-sudo apt install -y code libgtk2.0-0
+sudo apt install -y code libgtk2.0-0 exuberant-ctags cmake python-pip
+sudo pip install pylint autopep8
+
+code --install-extension patbenatar.advanced-new-file
+code --install-extension ms-vscode.cpptools
+code --install-extension twxs.cmake
+code --install-extension vector-of-bool.cmake-tools
+code --install-extension eamodio.gitlens
+code --install-extension bierner.markdown-preview-github-styles
+code --install-extension pkief.material-icon-theme
+code --install-extension ms-python.python
+code --install-extension slevesque.vscode-hexdump
+code --install-extension adammaras.overtype
+
 
