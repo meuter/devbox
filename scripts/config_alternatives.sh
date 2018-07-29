f282fa9a8807615dc1e5c183328284971cac20c2
Cédric Meuter <cedric.meuter@gmail.com>
2018-07-29 19:53:07 +0200

    vim.basic

--- scripts/config_alternatives.sh
+++ scripts/config_alternatives.sh
@@ -1,5 +1,5 @@
 #! /bin/bash -ex
 
-sudo update-alternatives --set editor /usr/bin/vim
+sudo update-alternatives --set editor /usr/bin/vim.basic
 sudo update-alternatives --set x-terminal-emulator /usr/bin/urxvt
 
