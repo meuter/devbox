1f8a29787ed35518917adb21c9e0b53212083180
Cédric Meuter <Cdric Meuter>
2018-07-29 12:48:00 +0200

    Need to install guest addition for vbox

--- scripts/install_guest_additions.sh
+++ scripts/install_guest_additions.sh
@@ -1,10 +1,9 @@
 #! /bin/bash -e
 
-sudo apt install -y dkms build-essential linux-headers-generic \
-                    linux-headers-$(uname -r) virtualbox-guest-additions-iso
+sudo apt install -y dkms build-essential linux-headers-generic linux-headers-$(uname -r) 
 
 sudo mkdir -p /media/cdrom
-sudo mount -o loop /usr/share/virtualbox/VBoxGuestAdditions.iso /media/cdrom
+sudo mount /dev/cdrom /media/cdrom
 sudo /media/cdrom/VBoxLinuxAdditions.run && true
 sudo umount /media/cdrom
 sudo rmdir /media/cdrom
