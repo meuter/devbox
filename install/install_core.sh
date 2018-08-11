14467f10cc21b2e7f0368185d4b59ec319251cdb
Cédric Meuter <cedric.meuter@gmail.com>
2018-08-11 08:05:16 +0000

    Removed unattended-upgrade by default

--- install/install_core.sh
+++ install/install_core.sh
@@ -7,8 +7,8 @@ sudo apt install -y build-essential neofetch python python-pip curl\
 	            dkms linux-headers-generic linux-headers-$(uname -r)
 
 info "Removing some bloatware..."
-sudo apt purge snapd ubuntu-core-launcher squashfs-tools\
-               lxde-settings-daemon nano
+sudo apt purge -y snapd ubuntu-core-launcher squashfs-tools\
+                  lxde-settings-daemon nano unattended-upgrades\
 
 success "Done!"
 
