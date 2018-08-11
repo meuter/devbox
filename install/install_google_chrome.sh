19f527e15ca859bb9630fff20bd8b6bfc9fd2855
Cédric Meuter <cedric.meuter@gmail.com>
2018-08-11 22:39:30 +0200

    added config for urxvt with nerdfont, but still no game

--- install/install_google_chrome.sh
+++ install/install_google_chrome.sh
@@ -5,7 +5,7 @@ source ./colors.sh
 info "Install Google Chrome..."
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-chrome.list
-sudo apt update 
+sudo apt update
 sudo apt install -y google-chrome-stable
 
 success "Done!"
